32139169daf3049f9caf1fe3a1832ed2a3ac96f7
Supercoder8888 <simonlqf@hotmail.com>
2024-03-10 18:00:14 +0800

    Add <ColorModeScript/> in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
@@ -9,6 +9,7 @@ import theme from "./theme";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
